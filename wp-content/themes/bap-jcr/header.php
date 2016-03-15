f3e73d3e99c0908bd669f0e02ae58adf3d33bc81
ArnaudDoubremelle <a-r-n-a-u-d-@hotmail.fr>
2016-03-15 22:43:24 +0100

    Modifs

--- wp-content/themes/bap-jcr/header.php
+++ wp-content/themes/bap-jcr/header.php
@@ -18,10 +18,10 @@
     <?php wp_head(); ?>
 </head>
 
-<body>
+<body <?php if (is_single() ):?>class="single"<?php endif; ?>>
 <!-- Header -->
 <header id="top" class="header">
     <nav id="navigation">
-        <?php wp_nav_menu(array('theme_location' => 'principal')); ?>
+        <!-- <?php wp_nav_menu(array('theme_location' => 'principal')); ?> -->
     </nav>
 </header>
