83bf2a69f98a4432cb895413c4de51d6c590562e
ArnaudDoubremelle <a-r-n-a-u-d-@hotmail.fr>
2016-03-16 15:25:39 +0100

    Modifs

--- wp-content/themes/bap-jcr/home.php
+++ wp-content/themes/bap-jcr/home.php
@@ -3,17 +3,13 @@
 <div class="container-fluid bacceuil">
     <div class="container">
         <div class="row">
-            <div class="col-md-12 mgt-50" id="video">
+            <div class="col-md-offset-2 col-md-8 mgt-50" id="video">
             </div>
             <div class="clearfix"></div>
             <div class="col-md-12 mgt-50 logo">
                 <img src="<?php bloginfo('template_directory') ?>/img/Logo_accueil_blanc.png" alt="logo" class="center-block img-responsive">
-            </div>
-            <div class="clearfix"></div>
-            <div class="col-md-12 mgt-50">
-                <nav class="center-block">
-                    <?php wp_nav_menu(array('theme_location' => 'principal')); ?>
-                </nav>
+                <a href="galerie" class="btn btn-primary">FR</a>
+                <a href="galerie" class="btn btn-danger">EN</a>
             </div>
         </div>
     </div>
