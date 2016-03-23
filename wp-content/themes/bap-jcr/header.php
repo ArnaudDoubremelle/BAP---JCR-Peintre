3f4cb80f8b524dceab00b7fcc91f3a6d53ba4813
ArnaudDoubremelle <a-r-n-a-u-d-@hotmail.fr>
2016-03-23 12:54:13 +0100

    menu

--- wp-content/themes/bap-jcr/header.php
+++ wp-content/themes/bap-jcr/header.php
@@ -21,7 +21,22 @@
 <body <?php if (is_single() ):?>class="single"<?php endif; ?>>
 <!-- Header -->
 <?php if (!is_page('home') ):?>
-    <div class="container">
-        <h1>Jean-Christophe Reneaux</h1>
+    <div id="wrapper" class="">
+        <div class="overlay" style="display: none;"></div>
+        <nav class="navbar navbar-inverse navbar-fixed-top" id="sidebar-wrapper" role="navigation">
+            <ul class="nav sidebar-nav">
+                <li>
+                    <a href="https://github.com/FragCoder/bootstrap-left-slide-menu"><i class="glyphicon glyphicon-camera"></i> Photo</a>
+                </li>
+                <li>
+                    <a href="https://github.com/FragCoder/bootstrap-left-slide-menu"><i class="glyphicon glyphicon-facetime-video"></i> Video</a>
+                </li>
+            </ul>
+        </nav>
+        <div id="page-content-wrapper">
+            <div class="container">
+                <h1><i class="icon-menu fa fa-bars" data-toggle="offcanvas"></i> Jean-Christophe Reneaux</h1>
+            </div>
+        </div>
     </div>
 <?php endif; ?>
--- wp-content/themes/bap-jcr/js/main.js
+++ wp-content/themes/bap-jcr/js/main.js
@@ -3,4 +3,27 @@ $(document).ready(function() {
     pause: true,
     interval: false,
   });
+
+  var trigger = $('.icon-menu'),
+      overlay = $('.overlay'),
+      isClosed = false;
+
+  function buttonSwitch() {
+
+    if (isClosed === true) {
+      overlay.hide();
+      isClosed = false;
+    } else {
+      overlay.show();
+      isClosed = true;
+    }
+  }
+
+  trigger.click(function () {
+    buttonSwitch();
+  });
+
+  $('[data-toggle="offcanvas"]').click(function () {
+    $('#wrapper').toggleClass('toggled');
+  });
 });
