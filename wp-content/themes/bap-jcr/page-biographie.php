c6f263138223ad2f629352a1eba8832b8faa0457
Alexis Bougy <alex.bj@orange.fr>
2016-04-05 10:17:31 +0200

    Fix responsive contact
    A corriger, soucis de responsive background de la page biogrpahie

--- wp-content/themes/bap-jcr/page-biographie.php
+++ wp-content/themes/bap-jcr/page-biographie.php
@@ -9,18 +9,22 @@ Template Name: biographie
     <div class="row">
         <?php get_template_part('menu-template');?>
     </div>
-    <div class="container backgroundbiographie mgt-80 ">
-        <div class="row">
-            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-                <div class="col-md-12 text-center mgt-80">
-                    <h2 class=""><?php the_title(); ?></h2>
-                </div>
-                <div class="clearfix"></div>
-                <div class="col-md-offset-1 col-md-10 text-center biographie mgt-50 pdb-50">
-                    <?php the_content(); ?>
-                </div>
-            <?php endwhile; endif; ?>
-        </div>
+    <div class="row">
+      <div class="col-xs-10 col-xs-offset-1 backgroundbiographie mgt-80 ">
+          <div class="row">
+              <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                  <div class="col-xs-12 text-center mgt-80">
+                      <h2 class=""><?php the_title(); ?></h2>
+                  </div>
+                  <div class="clearfix"></div>
+                  <div class="col-xs-8 col-xs-offset-2 text-center biographie mgt-50 pdb-50">
+                      <?php the_content(); ?>
+                  </div>
+              <?php endwhile; endif; ?>
+          </div>
+      </div>
+
     </div>
+
 </div>
 <?php get_footer()?>
