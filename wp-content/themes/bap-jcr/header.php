acea7177212408579d7424cf37b222a0f6fffd05
Sacha Bobinet <sachabobinet@gmail.com>
2016-04-20 15:40:49 +0200

    Yoast plugin

--- wp-content/themes/bap-jcr/header.php
+++ wp-content/themes/bap-jcr/header.php
@@ -4,7 +4,7 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
+    
 
     <title><?php bloginfo('name'); ?> - <?php the_title();?></title>
 
