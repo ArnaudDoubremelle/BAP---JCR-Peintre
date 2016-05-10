cc6f46c743dc01c8da86a5090364191fcd08ee82
ArnaudDoubremelle <a-r-n-a-u-d-@hotmail.fr>
2016-05-10 16:35:19 +0200

    new slider in category

--- wp-content/themes/bap-jcr/peintures.php
+++ wp-content/themes/bap-jcr/peintures.php
@@ -37,21 +37,5 @@ $my_query->the_post(); ?>
 <?php endwhile;
 wp_reset_postdata(); ?>
 
-<!-- Main -->
-<!--<div class="container-slider">-->
-<!--    <ul id="posts-slider">-->
-<!--        --><?php //$my_query = new WP_Query(array('post_type' => 'peintures')); ?>
-<!--        --><?php //while ($my_query->have_posts()) :
-//            $my_query->the_post(); ?>
-<!--            <li>-->
-<!--                <a href="--><?php //echo get_permalink(); ?><!--">--><?php //the_post_thumbnail('', array('class' => 'img-responsive')); ?><!--</a>-->
-<!--            </li>-->
-<!--        --><?php //endwhile;
-//        wp_reset_postdata(); ?>
-<!--    </ul>-->
-<!--</div>-->
-<!--<div class="clearfix"></div>-->
-<!--<div class="row">-->
-<!--    --><?php //get_template_part('template-contact');?>
-<!--</div>-->
+
 <?php get_footer(); ?>
